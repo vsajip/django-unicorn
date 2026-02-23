1d65abed95ff5c2aa895d7dbce8bcb2ea97cab30
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2026-02-23 19:11:11 +0000

    fix js test

--- tests/js/unicorn/init.test.js
+++ tests/js/unicorn/init.test.js
@@ -1,5 +1,28 @@
 import test from "ava";
+import { JSDOM } from "jsdom";
 import { init } from "../../../src/django_unicorn/static/unicorn/js/unicorn.js";
+import { components } from "../../../src/django_unicorn/static/unicorn/js/store.js";
+
+test.beforeEach(() => {
+  const dom = new JSDOM("<!doctype html><html><body></body></html>");
+  global.document = dom.window.document;
+  global.window = dom.window;
+  global.MutationObserver = dom.window.MutationObserver;
+  global.Node = dom.window.Node;
+  global.NodeFilter = dom.window.NodeFilter;
+
+  for (const key in components) {
+    delete components[key];
+  }
+});
+
+test.afterEach(() => {
+  delete global.document;
+  delete global.window;
+  delete global.MutationObserver;
+  delete global.Node;
+  delete global.NodeFilter;
+});
 
 test("init unicorn", (t) => {
   const actual = init("unicorn/", "X-Unicorn", "unicorn", { NAME: "morphdom" });
